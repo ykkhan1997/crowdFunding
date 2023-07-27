fcafe63693ba2ef02cabb2f7f7931b8d58a0efa8
Yasir Saeed <86898726+ykkhan1997@users.noreply.github.com>
2023-07-27 11:32:37 +0300

    third commit

--- Components/Footer.jsx
+++ Components/Footer.jsx
@@ -1,50 +1,51 @@
 import React from 'react'
 
 const Footer = () => {
-  const menuList=["Home","About","Contact","Blog"];
-  const contacList=["abc@gmail.com","def@cryptoKing.com","Contact Us"];
-  const usefulLinks=["Contact","Erc20","Erc721","Erc755"];
+  const menuList=["Home","About Us","Contact Us","Blog"];
+  const contactList=["support@cryptoking.com","info@cryptKing.com","Contact Us"];
+  const useFulLinks=["Erc20","Erc721","Erc1155","Funny Token"];
   return (
-   <div className='text-center backgroundMain dimWhite focus:text-white  lg:text-left'>
-    <div className='mx-6 py-10 text-center md:text-left'>
-      <div className='grid-1 grid gap-8 md:grid-cols-2 lg:grid-cols-4'>
-        <div>
-          <h6 className='mb-4 font-semibold uppercase justify-center md:justify-start'>Crypto King</h6>
-          <p>Here you can use rows and columns to organize your footer content.
+    <div className='backgroundMain text-center lg:text-left text-white'>
+      <div className='mx-4 py-10 text-center md:text-left'>
+        <div className='grid-1 grid gap-8 md:grid-cols-2 lg:grid-cols-4'>
+          <div>
+            <h6 className='font-semibold text-white tracking-wide mb-4 uppercase'>CryptoKing</h6>
+            <p className='dimWhite'>
+            Here you can use rows and columns to organize your footer content.
               Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-          </p>  
-        </div>
-        <div className=''>
-          <h6 className='mb-4 font-semibold uppercase justify-center md:justify-start'>Menu</h6>
-          {
-            menuList.map((menu,i)=>(
-             <p className='mb-4 font-semibold cursor-pointer'key={i+1}>{menu}</p>
-            ))
-          }
-        </div>
-        <div>
-          <h6 className='font-semibold justify-center uppercase md:justify-start mb-4'>Contact</h6>
-          {
-            contacList.map((contact,i)=>(
-              <p className='mb-4 font-semibold cursor-pointer'>{contact}</p>
-            ))
-          }
-        </div>
-        <div>
-          <h6 className='font-semibold uppercase mb-4 justify-center md:justify-start'>Userful Links</h6>
-          {
-            usefulLinks.map((link,i)=>(
-              <p key={i+1} className='font-semibold mb-4 cursor-pointer'>{link}</p>
-            ))
-          }
+            </p>
+          </div>
+          <div>
+            <h6 className='font-semibold uppercase text-white mb-4 tracking-wide md:justify-start'>Menu</h6>
+            {
+              menuList.map((menu,i)=>(
+                <p className='mb-4 font-medium tracking-wide dimWhite hover:text-white cursor-pointer' key={i+1}>{menu}</p>
+              ))
+            }
+          </div>
+          <div>
+            <h6 className='font-semibold uppercase text-white tracking-wide mb-4'>Contact Us</h6>
+            {
+              contactList.map((contact,i)=>(
+                <p key={i+1} className='mb-4 font-medium dimWhite hover:text-white cursor-pointer'>{contact}</p>
+              ))
+            }
+          </div>
+          <div>
+            <h6 className='font-semibold uppercase tracking-wide text-white mb-4'>Useful Links</h6>
+            {
+              useFulLinks.map((link,i)=>(
+                <p key={i+1} className='dimWhite hover:text-white font-medium mb-4 cursor-pointer'>{link}</p>
+              ))
+            }
+          </div>
         </div>
       </div>
+      <div className='p-4 text-center'>
+        <span>@2021Copyright:</span>
+        <a className='font-semibold' href='/'>CryptoKing</a>
+      </div>
     </div>
-    <div className='text-center p-6'>
-      <span>@2023 Copyright:</span>
-      <a className='font-semibold' href='/'>CryptoKing</a>
-    </div>
-   </div>
   )
 }
 
--- Components/Navbar.jsx
+++ Components/Navbar.jsx
@@ -1,123 +1,130 @@
-'use client'
-import React,{useState,useEffect,useContext} from 'react';
-import { CrowdFundingContext } from '@/Context/CrowdFunding';
-import {Logo,Menu,Close} from './page';
+"use client";
+import React, { useContext, useState } from "react";
+import Logo from "./Logo";
+import { CrowdFundingContext } from "@/Context/CrowdFunding";
+import Menu from "./Menu";
+import Close from "./Close";
 const Navbar = () => {
-  const {currentAccount,connectWallet,balance,copyContent} =useContext(CrowdFundingContext);
-  const [isMenuOpen,setIsMenuOpen]=useState(false);
-  const menuList=["Home","About Us","Contact Us","Blog"];
+  const menuList = ["Home", "About Us", "Contact Us", "Blog"];
+  const { currentAccount, connectWallet, balance } =
+    useContext(CrowdFundingContext);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
   return (
-    <div className='backgroundMain'>
-      <div className='px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-24 lg:px-8'>
-      <div className='relative flex justify-between items-center'>
-      <div className='flex items-center'>
-      <a
-      aria-label='Company'
-      title='Company'
-      href='/'
-      className='inline-flex items-center mr-8'>
-      <Logo color={`text-white`}/>
-      <span className='text-white ml-2 tracking-wide font-bold font-xl uppercase'>Company</span>
-      </a>
-      <ul className='lg:flex hidden space-x-8'>
-        {
-          menuList.map((el,i)=>(
-            <li key={i+1}>
-              <a className='dimWhite focus:text-white font-medium tracking-wide'
-              aria-label='Our Product'
-              title='Our Product'
-              href='/'>
-                {el}
-              </a>
-            </li>
-          ))
-        }
-      </ul>
-      </div>
-      <ul className='lg:flex hidden space-x-8'>
-        {
-          !currentAccount?
-          <li>
-            <button
-            aria-label='Open Menu'
-            title='Open Menu'
-            onClick={connectWallet}
-            className='text-white font-medium tracking-wide bg-blue-500 h-12 px-6 rounded shadow-sm hover:bg-blue-800'
+    <div className="backgroundMain">
+      <div className="px-4 py-5 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-xl md:px-20 lg:px-8">
+        <div className="relative flex items-center justify-between">
+          <div className="flex items-center">
+            <a
+              aria-label="Company"
+              title="Company"
+              href="/"
+              className="inline-flex items-center mr-8"
             >
-              Connect Wallet
+              <Logo color={`text-white`} />
+              <span className="ml-2 font-bold text-white uppercase tracking-wide">
+                Company
+              </span>
+            </a>
+            <ul className="lg:flex hidden space-x-8">
+              {menuList.map((menu, i) => (
+                <li key={i + 1}>
+                  <a
+                    aria-label="Our Product"
+                    title="Our Product"
+                    href="/"
+                    className="dimWhite hover:text-white font-medium tracking-wide"
+                  >
+                    {menu}
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </div>
+          <ul className="lg:flex hidden space-x-8">
+            {!currentAccount ? (
+              <button
+                onClick={connectWallet}
+                className="text-white bg-blue-500 hover:bg-blue-400 h-12 px-6 rounded shadow-sm font-medium"
+              >
+                Connect Wallet
+              </button>
+            ) : (
+              <button className="text-white bg-blue-400 h-12 px-6 rounded shadow-sm font-medium">
+                {balance.slice(0, 4)}Matic{" "}
+                <span className="bg-slate-500 p-2 rounded-2xl -mr-2">
+                  {currentAccount.slice(0, 6)}...{currentAccount.slice(39)}
+                </span>
+              </button>
+            )}
+          </ul>
+          <div className="lg:hidden z-40 overflow-hidden">
+            <button
+              aria-label="Open Menu"
+              title="Open Menu"
+              className="p-2 mr-1"
+              onClick={() => setIsMenuOpen(true)}
+            >
+              <Menu />
             </button>
-          </li>:
-          <button
-          className='h-12 bg-blue-500 inline-flex p-2 -mr-1 items-center bg-cover rounded'
-          >
-          <div className='text-white font-bold'>{balance.slice(0,4)} Matic</div>
-          <div className='bg-slate-200 ml-1 px-2 py-2 font-medium rounded'>{currentAccount.slice(0,6)}...{currentAccount.slice(39)}</div>
-          </button>
-        }
-      </ul>
-      <div className='lg:hidden flex z-40 overflow-hidden'>
-        <button
-        aria-label='Open Menu'
-        title='Open Menu'
-        href=''
-        className='p-2 mr-1'
-        onClick={()=>setIsMenuOpen(true)}>
-          <Menu/>
-        </button>
-        {
-          isMenuOpen && (
-            <div className='absolute top-0 left-0 w-full'>
-              <div className='bg-white p-5 rounded shadow-sm'>
-                <div className='flex items-center justify-between mb-4 overflow-hidden'>
-                  <div>
-                    <a
-                    aria-label='Company'
-                    title='Company'
-                    className='inline-flex items-center'>
-                      <Logo/>
-                      <span className='ml-2 tracking-wide text-xl uppercase font-bold'>Company</span>
-                    </a>
-                  </div>
-                  <div>
-                    <button
-                    aria-label='Close Menu'
-                    title='Close Menu'
-                    onClick={()=>setIsMenuOpen(false)}
-                    className='p-2 mt-2 mr-2  rounded bg-slate-200 hover:bg-slate-300'>
-                      <Close/>
-                    </button>
-                  </div>
-                </div>
-                <nav className='space-y-4'>
-                  {
-                    menuList.map((el,i)=>(
-                      <li key={i+1} className='list-none'>
-                        <a
-                        href='/'
-                        aria-label='Our Product'
-                        title='Our Product'
-                        className='font-medium tracking-wide text-gray-800 hover:text-gray-600'>
-                          {el}
-                        </a>
-                      </li>
-                    ))
-                  }
-                  <li className='list-none'>
-                    <button
-                    className='h-12 px-6 bg-blue-500 w-full hover:bg-blue-800 rounded shadow-sm text-white font-medium tracking-wide'>
-                      Connect Wallet
+            {isMenuOpen && (
+              <div className="absolute top-0 left-0 w-full">
+                <div className="bg-white p-5 rounded shadow-sm">
+                  <div className="flex items-center justify-between mb-4">
+                    <div>
+                      <a className="inline-flex items-center">
+                        <Logo />
+                        <span className="ml-2 tracking-wide uppercase text-xl font-bold">
+                          Company
+                        </span>
+                      </a>
+                    </div>
+                    <div>
+                      <button
+                        className="bg-slate-200 hover:bg-slate-300 p-1 rounded shadow-sm"
+                        onClick={() => setIsMenuOpen(false)}
+                      >
+                        <Close />
                       </button>
-                  </li>
-                </nav>
+                    </div>
+                  </div>
+                  <navbar>
+                    <ul className="space-y-4">
+                      {menuList.map((menu, i) => (
+                        <li key={i + 1}>
+                          <a
+                            aria-label="Our Product"
+                            title="Our Product"
+                            href=""
+                            className="font-medium tracking-wide text-gray-800 hover:text-gray-600"
+                          >
+                            {menu}
+                          </a>
+                        </li>
+                      ))}
+                      <li>
+                        {!currentAccount ? (
+                          <button
+                            onClick={connectWallet}
+                            className="h-12 px-6 bg-blue-500 hover:bg-blue-400 w-full rounded shadow-sm text-white font-bold tracking-wide"
+                          >
+                            Connect Wallet
+                          </button>
+                        ) : (
+                          <button className="bg-blue-500 hover h-12 px-6 rounded w-full items-center text-white font-bold">
+                            Your Connected With Wallet
+                          </button>
+                        )}
+                      </li>
+                    </ul>
+                  </navbar>
+                </div>
               </div>
-            </div>
-          )
-        }
-      </div>
-      </div>
+            )}
+          </div>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
